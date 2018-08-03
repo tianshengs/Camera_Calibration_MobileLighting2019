aacd5cfad899d8f706931f56424b5d04de49c4ef
tommaso-monaco <39607437+tommaso-monaco@users.noreply.github.com>
2018-08-03 11:53:46 -0400

    Add files via upload

--- src/calibration.h
+++ src/calibration.h
@@ -23,14 +23,6 @@ using namespace aruco;
 int calibrateWithSettings( const string inputSettingsFile );
 
 
-//global variables for AruCo calibration 
-vector< vector< Point2f > > allCornersConcatenated1;
-vector< int > allIdsConcatenated1;
-vector< int > markerCounterPerFrame1;
-vector< vector< Point2f > > allCornersConcatenated2; // and for stereo AruCo calibration
-vector< int > allIdsConcatenated2;
-vector< int > markerCounterPerFrame2;
-
 struct intrinsicCalibration {
   Mat cameraMatrix, distCoeffs;   //intrinsic camera matrices
   vector<Mat> rvecs, tvecs;       //extrinsic rotation and translation vectors for each image
@@ -38,9 +30,6 @@ struct intrinsicCalibration {
   vector<vector<Point3f> > objectPoints;  //corresponding 3d object points
   vector<float> reprojErrs;   //vector of reprojection errors for each pixel
   double totalAvgErr = 0;     //average error across every pixel
-  vector< vector< vector< Point2f > > > allCorners;
-  vector< vector< int > > allIds;
-  
 };
 
 struct stereoCalibration {
@@ -48,6 +37,19 @@ struct stereoCalibration {
     Mat R1, R2, P1, P2, Q;  //Rectification parameters ()
     Rect validRoi[2];       //Rectangle within the rectified image that contains all valid points
 };
+
+struct arucoCalibration {
+    vector<vector<Point2f> > imgPoints;   //corner points on 2d image 
+    vector<vector<Point3f> > objPoints;  //corresponding 3d object points prepared for calibration.
+    
+    vector< vector< vector< Point2f > > > allCorners; //all corners of detected markers
+    vector< vector< int > > allIds; //corresponding ids. 
+
+    vector< vector< Point2f > > allCornersConcatenated; //all detected corners concatenated per frame
+    vector< int > allIdsConcatenated; //corresponding ids
+    vector< int > markerCounterPerFrame; //number of images
+};
+
 
 class ChessBoard : public aruco::GridBoard  {
 
@@ -100,22 +102,28 @@ public:
     static bool fileCheck( const string& filename, FileStorage fs, const char * var);
     void saveIntrinsics(intrinsicCalibration &inCal);
     void saveExtrinsics(stereoCalibration &sterCal);
-
-
+    
+    
     // Properties
     Mode mode;
     Pattern calibrationPattern;   // One of the Chessboard, circles, or asymmetric circle pattern
 
+
+    //AruCo configuration
+    vector<int>  markersX;       // Number of AruCo Markers in first row
+    vector<int>  markersY;       // Number of AruCo Markers in first column 
+    vector<float> markerLength;  // The length of the markers in pixels
+    int dictionary;              // The number of the AruCo dictionary used to draw the markers
+    vector< int > type;          // The id of the first marker of the board
+    int numberOfBoards;          // Number of boards in the scene. Default:1
+
     // Chessboard settings
     Size boardSize;                 // The size of the board -> Number of items by width and height
-    float squareSize;               // The size of a square in your defined unit (point, millimeter,etc).
-
+    float squareSize;               // The size of a square in your defined unit (point, millimeter,etc)
+    
     // Input settings
     vector<string> imageList;
     string imageListFilename;
-
-    //vector <MarkerMap> configList; // Aruco config files
-    //string configListFilename;    // Input filename for aruco config files
 
     intrinsicCalibration intrinsicInput; // Struct to store inputted intrinsics
     string intrinsicInputFilename;    // Leave it at 0 to calculate new intrinsics
@@ -127,6 +135,7 @@ public:
 
     string undistortedPath;    // Path at which to save undistorted images (leave 0 to not save undistorted)
     string rectifiedPath;      // Path at which to save rectified images (leave 0 to not save rectified)
+    string detectedPath;       // Path at which to save images with detected patterns
 
     // Itrinsic calibration settings
     string fixDistCoeffs;              // A sequence of five digits (0 or 1) that
@@ -136,6 +145,16 @@ public:
     bool fixPrincipalPoint;         // Fix the principal point at the center
     int flag;                       // Flag to modify calibration
 
+    //Stereo Calibration settings
+    int alpha; // Free scaling factor during rectification
+    int rf;    // Resizing factor after rectification:
+               //  decides how large the resolution of the
+               //  image after rectification with respect to the original
+               //  image size.
+    
+    // LEAVE THIS VALUE AT "0" TO NOT CROP RECTIFIED IMAGES
+    int crop;  // Crops the final rectified images
+    
     // UI settings
     bool showUndistorted;         // Show undistorted images after intrinsic calibration
     bool showRectified;           // Show rectified images after stereo calibration
